bb7cd512f32ed34b3b5af2a520e7a765a908d8c5
LiuHaoji <lhj@qq.com>
2024-05-13 20:48:51 +0800

    update

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -8,8 +8,8 @@ import torch
 import yaml
 from tqdm import tqdm
 
-from utils import TryExcept
-from utils.general import LOGGER, TQDM_BAR_FORMAT, colorstr
+from detector.neural_networks.yolov5.utils import TryExcept
+from detector.neural_networks.yolov5.utils.general import LOGGER, TQDM_BAR_FORMAT, colorstr
 
 PREFIX = colorstr("AutoAnchor: ")
 
